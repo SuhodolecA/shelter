bfba05fc475f05a3ad4b03a0b565f2c1f56e6488
Anton Sukhadolets <suhodoleca@gmail.com>
2023-02-01 16:01:29 +0100

    refactor: update hamburger menu js code

--- js/script.js
+++ js/script.js
@@ -6,19 +6,36 @@ const pageOverlay = document.querySelector("#page-overlay");
 const header = document.querySelector("#header-section");
 const body = document.querySelector("body");
 
+// helper functions
+const showMenu = () => {
+  navigationMenu.classList.remove("slide-out");
+  navigationMenu.classList.add("slide-in");
+};
+
+const closeMenu = () => {
+  navigationMenu.classList.remove("slide-in");
+  navigationMenu.classList.add("slide-out");
+};
+
+const addOverlay = () => {
+  pageOverlay.classList.remove("hide");
+  body.classList.add("stop-scroll");
+};
+
+const removeOverlay = () => {
+  pageOverlay.classList.add("hide");
+  body.classList.remove("stop-scroll");
+};
+
 // burger menu
 hamburgerBtn.addEventListener("click", () => {
   if (hamburgerBtn.classList.contains("rotate")) {
     hamburgerBtn.classList.remove("rotate");
-    pageOverlay.classList.add("hide");
-    body.classList.remove("stop-scroll");
-    navigationMenu.classList.remove("slide-in");
-    navigationMenu.classList.add("slide-out");
+    removeOverlay();
+    closeMenu();
   } else {
     hamburgerBtn.classList.add("rotate");
-    pageOverlay.classList.remove("hide");
-    body.classList.add("stop-scroll");
-    navigationMenu.classList.remove("slide-out");
-    navigationMenu.classList.add("slide-in");
+    addOverlay();
+    showMenu();
   }
 });
