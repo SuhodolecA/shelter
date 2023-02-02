e819c9704790475036566837f1c0a7bbcf1f9c78
Anton Sukhadolets <suhodoleca@gmail.com>
2023-02-02 11:02:33 +0100

    feat: add some features for mobile menu and resolve some issues

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const header = document.querySelector("#header-section");
 const body = document.querySelector("body");
 
 // helper functions
+// ==========================================
 const showMenu = () => {
   navigationMenu.classList.remove("slide-out");
   navigationMenu.classList.add("slide-in");
@@ -26,6 +27,11 @@ const removeOverlay = () => {
   pageOverlay.classList.add("hide");
   body.classList.remove("stop-scroll");
 };
+const removeSlideInOutAnimation = () => {
+  navigationMenu.classList.remove("slide-in");
+  navigationMenu.classList.remove("slide-out");
+};
+// ================================================
 
 // burger menu
 hamburgerBtn.addEventListener("click", () => {
@@ -40,9 +46,61 @@ hamburgerBtn.addEventListener("click", () => {
   }
 });
 
+// navigation links behavior
+navigationMenuLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    navigationMenuLinks.forEach((link) => {
+      link.classList.remove("current-link");
+    });
+    const currentLink = event.target;
+    currentLink.classList.add("current-link");
+    if (navigationMenu.classList.contains("mobile-menu")) {
+      hamburgerBtn.classList.remove("rotate");
+      closeMenu();
+      removeOverlay();
+    }
+  });
+});
+
 //overlay
 pageOverlay.addEventListener("click", () => {
   hamburgerBtn.classList.remove("rotate");
   removeOverlay();
   closeMenu();
+});
+
+// window resize(remove side animation from hamburger menu for better view on different sizes)
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 768) {
+    removeSlideInOutAnimation();
+    navigationMenu.classList.remove("mobile-menu");
+    hamburgerBtn.classList.remove("rotate");
+    removeOverlay();
+  } else {
+    navigationMenu.classList.add("mobile-menu");
+  }
+});
+
+// window scroll (add header position fixed depend of width)
+window.addEventListener("scroll", () => {
+  if (
+    (window.innerWidth >= 320 &&
+      window.innerWidth < 768 &&
+      window.scrollY >= 719) ||
+    (window.innerWidth >= 768 && window.scrollY >= 1140) ||
+    (window.innerWidth >= 1280 && window.scrollY >= 890)
+  ) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+});
+
+// add|remove mobile-menu class for proper mobile menu links work
+window.addEventListener("load", () => {
+  if (window.innerWidth >= 768) {
+    navigationMenu.classList.remove("mobile-menu");
+  } else {
+    navigationMenu.classList.add("mobile-menu");
+  }
 });
