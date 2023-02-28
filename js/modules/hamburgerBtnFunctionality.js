bb7a0b50cbc6644acb7fc210b00e29df0be42e4b
Anton Sukhadolets <suhodoleca@gmail.com>
2023-02-28 15:31:30 +0100

    feat: add basic accessible hamburger menu behavior

--- js/modules/hamburgerBtnFunctionality.js
+++ js/modules/hamburgerBtnFunctionality.js
@@ -10,12 +10,14 @@ const hamburgerBtnFunctionality = () => {
   hamburgerBtn.addEventListener("click", () => {
     if (hamburgerBtn.classList.contains("rotate")) {
       hamburgerBtn.classList.remove("rotate");
+      hamburgerBtn.setAttribute("aria-expanded", "false");
       removeOverlay();
       closeMenu();
     } else {
       hamburgerBtn.classList.add("rotate");
       addOverlay();
       showMenu();
+      hamburgerBtn.setAttribute("aria-expanded", "true");
     }
   });
 };
